f11c4337ea6c1cee97cb3a1db37ad2a91af68c69
milena <milenaprado1020@gmail.com>
2023-03-23 16:33:35 -0300

    poo

--- poo/pessoa.py
+++ poo/pessoa.py
@@ -1,11 +1,19 @@
-class pessoa:
+import json
+
+class Pessoa:
     def __init__(self, nome, idade):
         self.nome = nome
         self.idade = idade
     
-
+caminho = 'pessoa1.json'
 maria = {'nome': 'maria', 'idade': 20}
-p1 = pessoa(**maria)
+p1 = Pessoa('maria',20)
+p2 = Pessoa('joão',30)
+pe = [vars(p1),vars(p2)]
+with open (caminho, 'w', encoding='utf8') as arquivo: 
+    json.dump(pe, 
+              arquivo,
+              ensure_ascii=False,
+              indent=2,
+            )
 
-print(p1.nome)
-print(p1.idade)
---
+++ poo/pessoa_a.py
@@ -0,0 +1,11 @@
+import json
+from pessoa import caminho, Pessoa
+
+with open(caminho, 'r') as arquivo:
+    pessoas = json.load(arquivo)
+    p1 = Pessoa(**pessoas[0])
+    p2 = Pessoa(**pessoas[1])
+
+    print(p1.nome, p1.idade)
+    print(p2.nome, p2.idade)
+
