d45f8b1f8b3afe10fc5d4a016d4e3d138fd4bd7a
milena <milenaprado1020@gmail.com>
2023-05-01 20:25:41 -0300

    sqlite3

--- section8/main.py
+++ section8/main.py
@@ -4,9 +4,43 @@ from pathlib import Path
 ROOT_DIR = Path(__file__). parent
 DB_NAME = 'db.sqlite3'
 DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
 
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
+cursor.execute(
+    f'DELETE FROM {TABLE_NAME}'
+)
+cursor.execute(
+    f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
+)
+connection.commit()
+
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weight REAL'
+    ')'
+)
+connection.commit()
+
+sql = (
+    f'INSERT INTO {TABLE_NAME} '
+    '(name, weight) '
+    'VALUES '
+    '(?,?)'
+)
+# cursor.execute(sql,['maria', 5])
+cursor.executemany(sql,[['maria', 5], ['joão', 4]])
+connection.commit()
+
+
 cursor.close()
-connection.closer()
+connection.close()
+
+
+if __name__ == '__main__':
+    print(sql)
